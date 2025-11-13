211929c4e35cfae1d1495b6c1246c87500bbea32
RKJ <jrohit1110@gmail.com>
2025-11-12 22:44:08 -0500

    Nice g2 at 2.5m/s
    Fixed COT Calculation

--- src/robots/robot.py
+++ src/robots/robot.py
@@ -281,8 +281,11 @@ class Robot:
       if self._gym.query_viewer_has_closed(self._viewer):
         sys.exit()
 
+      # For view from right side
       mean_pos = torch.min(self.base_position_world,
-                           dim=0)[0].cpu().numpy() + np.array([-2.5, -2.5, 2.5])
+                           dim=0)[0].cpu().numpy() + np.array([0., -2.0, 0.2])  # Side view (no X offset, positive Y)
+
+
       # mean_pos = torch.min(self.base_position_world,
       #                      dim=0)[0].cpu().numpy() + np.array([0.5, -1., 0.])
       target_pos = torch.mean(self.base_position_world,
@@ -340,6 +343,17 @@ class Robot:
   @property
   def base_velocity_world_frame(self):
     return self._base_lin_vel_world
+
+  @property
+  def base_vel(self):
+    return torch.clone(self._base_lin_vel_world)
+
+  @property
+  def mass(self):
+    # Get mass for the first environment (all envs have same robot)
+    body_props = self._gym.get_actor_rigid_body_properties(self._envs[0], self._actors[0])
+    total_mass = sum([prop.mass for prop in body_props])
+    return total_mass
 
   @property
   def base_velocity_body_frame(self):
