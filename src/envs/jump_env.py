fdea2f178785b63ebf3ac1f38252844c4dd0b384
rohit-kumar-j <jrohit1110@gmail.com>
2026-02-09 18:53:06 -0500

    x

--- src/envs/jump_env.py
+++ src/envs/jump_env.py
@@ -628,7 +628,8 @@ class JumpEnv:
         desired_foot_positions += foot_action_world
 
       # DEBUG: View projected rectagles on the ground
-      if self._show_gui and step == 0:  # Only draw once per env.step(), not every control loop iteration
+      # if self._show_gui and step == 0:  # Only draw once per env.step(), not every control loop iteration
+      if (self._show_gui or self._record_video) and step == 0:
         self._gym.clear_lines(self._viewer)
         
         # Draw desired foot landing positions
