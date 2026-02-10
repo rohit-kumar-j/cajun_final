b65d60aedc17fda6a2d2dfb537a22ed626806b53
rohit-kumar-j <jrohit1110@gmail.com>
2026-02-09 21:27:09 -0500

    Added g2

--- src/envs/go1_rewards.py
+++ src/envs/go1_rewards.py
@@ -11,12 +11,27 @@ class Go1Rewards:
     self._num_envs = self._env.num_envs
     self._device = self._env.device
 
-  def speed_tracking_reward(self):
+  def speed_tracking_reward_old(self):
     actual_speed = torch.concatenate(
         (self._robot.base_velocity_body_frame[:, :2],
          self._robot.base_angular_velocity_body_frame[:, 2:]),
         dim=1)
     return -torch.sum(torch.square(self._env.command - actual_speed), dim=1)
+
+  def speed_tracking_reward(self):
+    # Construct actual: [vx, vy, yaw_rate]
+    actual = torch.cat([
+        self._robot.base_velocity_body_frame[:, :2],  # vx, vy
+        self._robot.base_angular_velocity_body_frame[:, 2:3]  # yaw_rate
+    ], dim=1)
+      
+    # Construct desired: [vx, vy, yaw_rate]
+    desired = torch.cat([
+      self._env._desired_velocity[:, :2],  # vx, vy from desired_velocity
+      torch.zeros(self._num_envs, 1, device=self._device)  # zero yaw_rate
+    ], dim=1)
+      
+    return -torch.sum(torch.square(desired - actual), dim=1)
 
   def forward_speed_reward(self):
     return self._robot.base_velocity_body_frame[:, 0]
